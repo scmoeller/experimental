71f4a9d3d6a9bb8ddfcad744262bce882286a094
Stefan C. Möller <moeller@ift-nord.de>
2014-12-18 14:50:03 +0100

    Initialisierung des Projekts.

---
+++ src/Scmoeller/Experimental/Closures/closure.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Scmoeller\Experimental\Closures;
+
+// Anonyme Funktion zum Ausgaben des Namens.
+$echo = function($name) {
+    echo $name, "\n";
+};
+
+$echo('Stefan C. Möller');
+
+
